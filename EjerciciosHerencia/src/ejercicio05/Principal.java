e559de4e6e8c3697dac97acdc696d25077bff991
raulromerap <raul.romera@iesnervion.es>
2024-04-10 17:19:55 +0200

    10/04/2024

--- EjerciciosHerencia/src/ejercicio05/Principal.java
+++ EjerciciosHerencia/src/ejercicio05/Principal.java
@@ -1,32 +1,23 @@
 package ejercicio05;
 
-import java.util.Scanner;
-
 public class Principal {
 
-	private static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		Poligono p1 =new Rectangulo(2,154,12);
+		System.out.println(p1);
 		
-		int opc = 0;
-		double lado1 = 0;
-		double lado2 = 0;
-		double lado3 = 0;
-		menu();
-		opc = sc.nextInt();
-		do {
-		sc.nextLine();
-		switch(opc) {
-		case 1: 
-			System.out.println("Introduce el valor del lado1");
-			
-			break;
-		}
-		}while(opc != 0);
+		Poligono p2 = new Triangulo(3,23,13,24);
+		System.out.println(p2);
+
+	}
+	/**
+	 * Metodo para imprimir el menu
+	 */
+	static void menu(){
+		System.out.println("Elige una de las siguientes opciones:");
+		System.out.println("1. Introducir un rectangulo");
+		System.out.println("2. Introducir un triangulo");
+		System.out.println("3. Listar el contenido del array");
 	}
 
-	public static void menu() {
-		System.out.println("1. Introducir triangulo");
-		System.out.println("2. Introducir rectangulo");
-		System.out.println("3. Mostrar contenido");
-	}
 }
